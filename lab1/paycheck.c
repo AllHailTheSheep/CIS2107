10011f0e1f096776d9b0dc5ac9fe94522bddab3c
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-01 16:30:06 -0500

    add tabs to paycheck.c

--- lab1/paycheck.c
+++ lab1/paycheck.c
@@ -9,9 +9,9 @@ const char* line = "=====================================";
 
 int main() {
 	printf("%s\n", "Welcome to \"TEMPLE HUMAN RESOURCES\"");
-	int eID = getInt("Enter Employee ID: ", "This is not a valid Employee Number.\nPlease run the program again.");
-	double hourly = getDouble("Enter Hourly Salary: ", "This is not a valid hourly salary amount.\nPlease run the program again.");
-	double weekly = getDouble("Enter Weekly Hours: ", "This is not a weekly time.\nPlease run the program again.");
+	int eID = getInt("\tEnter Employee ID: ", "This is not a valid Employee Number.\nPlease run the program again.");
+	double hourly = getDouble("\tEnter Hourly Salary: ", "\tThis is not a valid hourly salary amount.\nPlease run the program again.");
+	double weekly = getDouble("\tEnter Weekly Hours: ", "\tThis is not a weekly time.\nPlease run the program again.");
 	calculate(eID, hourly, weekly);
 	printf("%s\n", "Thank you for using \"TEMPLE HUMAN RESOURCES\"");
 	return EXIT_SUCCESS;
@@ -40,17 +40,17 @@ int getInt(char* prompt, char* onFail) {
 }
 
 void calculate(int eID, double hourly, double weekly) {
-	printf("%s\n", line);
-	printf("Employee #: %d\n", eID);
-	printf("Hourly Salary: $%.2f\n", hourly);
-	printf("Weekly Time: %.2f\n", weekly);
+	printf("\t%s\n", line);
+	printf("\tEmployee #: %d\n", eID);
+	printf("\tHourly Salary: $%.2f\n", hourly);
+	printf("\tWeekly Time: %.2f\n", weekly);
 	double regular, overtime, net;
 	regular = weekly > 40 ? hourly * 40 : weekly * hourly;
 	
 	overtime = weekly > 40 ? (weekly - 40) * (1.5 * hourly) : 0;
 	net = regular + overtime;
-	printf("Regular Pay: $%.2f\n", regular);
-	printf("Overtime: $%.2f\n", overtime);
-	printf("Net Pay: $%.2f\n", net);
-	printf("%s\n", line);	
+	printf("\tRegular Pay: $%.2f\n", regular);
+	printf("\tOvertime: $%.2f\n", overtime);
+	printf("\tNet Pay: $%.2f\n", net);
+	printf("\t%s\n", line);	
 }
