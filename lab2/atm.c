05c308aea9cadcb0100ea459577de58ef1f4c418
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-09 22:23:49 -0500

    continue lab 2

--- lab2/atm.c
+++ lab2/atm.c
@@ -7,10 +7,10 @@ double getDoubleR(char*, char*, int);
 int getInt(char*, char*);
 int getIntR(char*, char*, int);
 void printOptions();
-int getIntAndValidate(char*, char*, const int*, size_t);
-bool valueInArray(int, const int*, size_t);
+int getIntAndValidate(char*, char*, int*, size_t);
+bool valueInArray(int, int*, size_t);
 
-int const OPTIONS[4] = {1, 2, 3, 4};
+int  OPTIONS[4] = {1, 2, 3, 4};
 
 int main() {
     int pin;
@@ -49,7 +49,7 @@ double getDoubleR(char* prompt, char* onFail, int fails) {
 	return i;
 }
 
-bool valueInArray(int val, const int *arr, size_t n) {
+bool valueInArray(int val, int *arr, size_t n) {
     // This function was borrowed from Dimitroff's answer to this StackOverflow question: https://stackoverflow.com/questions/39742123/check-if-value-is-already-present-in-array
     for(size_t i = 0; i < n; i++) {
         if(arr[i] == val)
@@ -58,7 +58,7 @@ bool valueInArray(int val, const int *arr, size_t n) {
     return false;
 }
 
-int getIntAndValidate(char* prompt, char* onFail, const int* valid, size_t size) {
+int getIntAndValidate(char* prompt, char* onFail, int* valid, size_t size) {
     int res = getIntR(prompt, onFail, 0);
     if (valueInArray(res, valid, size)) {
         printf("g%s\n", "Not a valid option. Please try again.");
