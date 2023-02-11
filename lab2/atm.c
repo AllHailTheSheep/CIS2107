05fa0e93d2869e82f2d549e7add0063af04c96ba
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-10 19:39:03 -0500

    continue lab 2

--- lab2/atm.c
+++ lab2/atm.c
@@ -3,13 +3,13 @@
 #include <stdbool.h>
 
 double getDouble(char*, char*);
-double getDoubleR(char*, char*, int);
-int getInt(char*, char*);
-int getIntR(char*, char*, int);
+int getPIN(char*, char*);
+int getIntR(char*, char*);
 void printOptions();
 int getIntAndValidate(char*, char*, int*, size_t);
 
 int  OPTIONS[4] = {1, 2, 3, 4};
+int const MAX_FAILS = 3;
 
 int main() {
     int pin;
@@ -18,7 +18,7 @@ int main() {
     pin = getInt("Enter your PIN: ", "Not a valid PIN.");
     while (!QUIT) {
         printOptions();
-        int option = getIntAndValidate("Choose an option: ", "", OPTIONS, 4);
+        int option = getInt("Choose an option: ", "Not a valid option.");
         printf("%d\n", option);
         // TODO: switch statement instead of ifs
         if (option == 4) {
@@ -29,58 +29,33 @@ int main() {
 
 }
 
-double getDouble(char* prompt, char* onFail) {
-    getDoubleR(prompt, onFail, 0);
+int getOption() {
+    int fails = 0;
+    return -1;
 }
 
-double getDoubleR(char* prompt, char* onFail, int fails) {
+int getPIN(char* prompt, char* onFail) {
+    int fails = 0;
+    while (fails < MAX_FAILS) {
+        int res = getIntR(prompt, onFail);
+        if (res < 0) {
+            fails++;
+        } else {
+            return res;
+        }
+    }
+    printf("%d", "To many failures. Please try again.");
+}
+
+int getIntR(char* prompt, char* onFail) {
 	double i;
 	printf("%s", prompt);
 	scanf("%lf", &i);
-	if (i <= 0 && fails < 2) {
-		printf("%s\n", onFail);
-		i = getDoubleR(prompt, onFail, ++fails);
+	if (i != (int)i) {
+		printf("\n%s\n", onFail);
+		return -1;
 	}
-    if (fails >= 3) {
-        printf("%s\n", onFail);
-        printf("%s\n", "Too may attempts. Exiting...\n");
-        exit(EXIT_FAILURE);
-    }
-	return i;
-}
-
-int getIntAndValidate(char* prompt, char* onFail, int* valid, size_t size) {
-    int res = getInt(prompt, onFail);
-    bool flag = false;
-    for (size_t i = 0; i < size; i++) {
-        if (valid[i] == res) {
-            flag = true;
-        }
-    }
-    if (!flag) {
-        res = getIntAndValidate(prompt, onFail, valid, size);
-    }
-    return res;
-}
-
-int getInt(char *prompt, char* onFail) {
-    return getIntR(prompt, onFail, 0);
-}
-
-int getIntR(char* prompt, char* onFail, int fails) {
-	int i;
-	printf("%s", prompt);
-	scanf("%d", &i);
-	if (i <= 0 && fails < 2) {
-		printf("%s\n", onFail);
-		i = getIntR(prompt, onFail, ++fails);
-	}
-    if (fails >= 3) {
-        printf("%s\n", onFail);
-        printf("%s\n", "Too many attempts. Exiting..\n");
-        exit(EXIT_FAILURE);
-    }
-	return i;
+	return (int)i;
 }
 
 void printOptions() {
