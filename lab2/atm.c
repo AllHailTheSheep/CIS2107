edb0bcd96a3e5271515623f90ecf89500318e867
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-11 16:22:22 -0500

    continue lab 2

--- lab2/atm.c
+++ lab2/atm.c
@@ -39,8 +39,7 @@ int main() {
             break;
         case 4: ;
             // quit
-            int i;
-            for(i = actionscount; i > -1; i++) {
+            for(int i = 0; i < actionscount; i++) {
                 printf("%d\n", actions[i]);
             }
             QUIT = true;
