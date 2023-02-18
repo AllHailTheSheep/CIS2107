49c67ea6d13cdfce89a755aea955d74d168fc899
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-18 17:50:54 -0500

    gcc why you no like me

--- lab3/Arrays1DDemo.c
+++ lab3/Arrays1DDemo.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <limits.h>
+#include <string.h>
 
 void printArray(int*, size_t);
 void fillArray(int*, size_t, int, int);
+int findWithRange(int*, size_t, int, int);
+void reverseArray(int*, size_t);
 
 int main() {
-    int arr0[6];
-    fillArray(arr0, 6, 0, 10);
-    printArray(arr0, 6);
-    int arr1[100];
-    fillArray(arr1, 100, 0, 100);
-    printArray(arr1, 100);
+    int arr[100];
+    // fillArray demo
+    fillArray(arr, 100, 0, 100);
+    printArray(arr, 100);
+
+    // findWithRange demo
+    // will fail because min is less than 0
+    // findWithRange(arr, 100, -1, 40);
+    // will fail because max is greater than the size of the array
+    // findWithRange(arr, 100, 0, 100);
+    // will fail because mindex == maxdex
+    // findWithRange(arr, 100, 1, 1);
+    // note that the mindex and maxdex params are interchangeable
+    printf("max from 0-25: %d\n", findWithRange(arr, 100, 0, 25));
+    printf("max from 25-0: %d\n", findWithRange(arr, 100, 25, 0));
+    printf("max from 90-99: %d\n", findWithRange(arr, 100, 90, 99));
+    
+    // reverseArray demo
+    reverseArray(arr, sizeof(arr));
+    printArray(arr, sizeof(arr));
 }
 
 void printArray(int* arr, size_t size) {
@@ -25,6 +43,34 @@ void printArray(int* arr, size_t size) {
 void fillArray(int* arr, size_t size, int rand_min, int rand_max) {
     for (size_t i = 0; i < size; i++) {
         arr[i] = rand() % (rand_max - rand_min + 1) + rand_min;
+    }
+    return;
+}
+
+int findWithRange(int* arr, size_t size, int mindex, int maxdex) {
+    if (mindex > maxdex) {
+        int swp = mindex;
+        mindex = maxdex;
+        maxdex = swp;
+    }
+    if (mindex < 0 || maxdex > size || mindex == maxdex) {
+        printf("%s\n", "Minimum must be greater than 0 and max must be less than the size of the array!");
+        exit(EXIT_FAILURE);
+    }
+    int max = INT_MIN;
+    for (int i = mindex; i <= maxdex; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void reverseArray(int* arr, size_t size) {
+    int new[size];
+    memcpy(new, arr, size);
+    for (int i = 0; i < size; i++) {
+        arr[size - 1 - i] = arr[i];
     }
     return;
 }
