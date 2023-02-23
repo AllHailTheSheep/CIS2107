08414590979e32cdf364144d7eb66b0f56dfb2eb
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-23 15:55:27 -0500

    start lab4

---
+++ lab4/Arrays2D.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
+
+const int ROW = 3;
+const int COL = 3;
+
+int main() {
+    // TODO: take user input for row and col
+    int arr[]
+    
+}
+
+void fill2DArray(int* arr, int row, int col, bool rand) {
+
+}
