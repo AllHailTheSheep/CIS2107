ad75ffcc564aec37f4facd555e8f4a8d894ecf34
AllHailTheSheep <juli4fasick@gmail.com>
2023-02-23 20:49:15 -0500

    continue lab5

--- lab5/Arrays2D.c
+++ lab5/Arrays2D.c
@@ -5,12 +5,19 @@
 #include <string.h>
 #include <stdbool.h>
 
+void fill2DArray(int*, int, int, bool);
+
 const int ROW = 3;
 const int COL = 3;
 
 int main() {
     // TODO: take user input for row and col
-    int arr[]
+    int arr[ROW][COL];
+    int (*pointer)[ROW][COL];
+    pointer = &arr;
+
+    fill2DArray(pointer, ROW, COL, false);
+
     
 }
 
